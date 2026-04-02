671c4d046203758549217c6ff19fda4d3b4bcfcd
Kevalarmano <keval4ram@gmail.com>
2026-04-02 23:28:19 +0000

    feat: add protected route for project creation page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Dashboard from "./pages/dashboard/Dashboard";
+import CreateProject from "./pages/projects/CreateProject";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <div className="bg-black text-white min-h-screen">
         <Routes>
+          {/* Protected Dashboard */}
           <Route
             path="/"
             element={
@@ -19,6 +21,17 @@ function App() {
             }
           />
 
+          {/* Protected Create Project */}
+          <Route
+            path="/create"
+            element={
+              <ProtectedRoute>
+                <CreateProject />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Public Routes */}
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
