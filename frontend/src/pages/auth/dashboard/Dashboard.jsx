3977d51f71e51b6012f78ea486fa1c92cf77ab18
Kevalarmano <keval4ram@gmail.com>
2026-04-02 23:23:31 +0000

    feat: implement user logout functionality in dashboard

--- frontend/src/pages/auth/dashboard/Dashboard.jsx
+++ frontend/src/pages/auth/dashboard/Dashboard.jsx
@@ -1,15 +1,35 @@
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
+import { signOut } from "firebase/auth";
+import { auth } from "../../services/firebase";
 
 function Dashboard() {
   const { user } = useContext(AuthContext);
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      alert("Logged out successfully");
+    } catch (error) {
+      alert(error.message);
+    }
+  };
 
   return (
     <div className="p-6">
       <h1 className="text-2xl text-green-500">Dashboard</h1>
 
       {user ? (
-        <p>Welcome, {user.email}</p>
+        <>
+          <p>Welcome, {user.email}</p>
+
+          <button
+            onClick={handleLogout}
+            className="bg-red-500 px-4 py-2 mt-4 rounded"
+          >
+            Logout
+          </button>
+        </>
       ) : (
         <p>You are not logged in</p>
       )}
